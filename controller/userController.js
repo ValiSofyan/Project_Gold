86a26f13446d6810665743667741708d4894686d
valisofyan <valisofyan@gmail.com>
2023-10-29 15:11:49 +0700

    commit branch develop[ment

--- controller/userController.js
+++ controller/userController.js
@@ -1,50 +1,41 @@
-const express = require("express");
 const dbUser = require("../db/userData.json")
-const app = express()
 const fs = require("fs")
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
 class userController{
-    static cekapi (req,res){
-    try {
-        let idDynamic;
-        if (dbUser.length > 0) {
-            idDynamic = dbUser[dbUser.length - 1].id + 1;
-        } else {
-            idDynamic = 1; 
-        }
+    static inputDataUser (req,res){
+        try {
+            let idDynamic;
+            if (dbUser.length > 0) {
+                idDynamic = dbUser[dbUser.length - 1].id + 1;
+            } else {
+                idDynamic = 1; 
+            }
 
-        const {userName,password,skill} = req.body
-        let tampData={
-            id : idDynamic,
-            userName,
-            password,
-            skill,
-         }
+            const {userName,password,skill} = req.body
+            if (!userName || !password || !skill) {
+                throw new Error("Masukan yang benar"); 
+            }
+            let tampData={
+                id : idDynamic,
+                userName,
+                password,
+                skill,
+            }
 
-        console.log(tampData);
-        dbUser.push(tampData)
-        let manipulateDataTampData = JSON.stringify(dbUser)
+            console.log(tampData);
+            dbUser.push(tampData)
+            let manipulateDataTampData = JSON.stringify(dbUser)
         
-        fs.writeFileSync("./db/userData.json", manipulateDataTampData)
-        res.status(201).json("SUCCESS to ADD")
+            fs.writeFileSync("./db/userData.json", manipulateDataTampData)
+            res.status(201).json("SUCCESS to ADD")
         
         }catch (error) {
             console.error(error);
-            res.status(500).json(error.message);
+            res.status(400).json(error.message);
         } 
 
     }
 }
-gitignore
-        README.md
-        app.js
-        controller/
-        db/
-        package-lock.json
-        package.json
-        routes/
+
 
 module.exports = userController
--- routes/userCorpRoutes.js
+++ routes/userCorpRoutes.js
@@ -1,6 +1,6 @@
 const routes = require ("express").Router()
 const userCorpController = require("../controller/userCorpController")
 
-routes.post("/reg1", userCorpController.cekapi1)
+routes.post("/reg1", userCorpController.inputDataCorp)
 
 module.exports = routes
